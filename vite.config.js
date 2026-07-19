5eef24970729f85224570376cfb27c26d64764ba
tanjimjoy <tanjimjoy@gmail.com>
2026-07-19 23:24:01 +0600

    done

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    host: '0.0.0.0',
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8081',
         changeOrigin: true,
       },
     },
